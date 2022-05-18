9bc0cdb5de98eab0c4eac2a50bbbab08a9ab73c3
jrenzoo <u202118568@upc.edu.pe>
2022-05-18 11:05:07 -0500

    fix: grid component in sign in page

--- src/security/pages/sign-in.component.vue
+++ src/security/pages/sign-in.component.vue
@@ -1,37 +1,53 @@
 <template>
-<div class="m-3 my-8 flex flex-column align-items-center justify-content-center">
-    <h1 class="text-center text-6xl">Sign In</h1>
-    <div class="m-3 text-center">
-        <div class="px-3">
-            <span class="p-inputgroup-addon inline">
-                <i class="pi pi-user"></i>
-            </span>
-            <pv-input-text placeholder="Username" class="pr-8" />
-        </div>
+<div class="grid min-h-screen mr-0">
+    <div class="col-12 md:col-6 p-0">
+        <img class="w-full h-full" src="https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980" alt="man diriving a car">
     </div>
+    <div class="col-12 md:col-6 p-0 flex justify-content-center">
+        <div class="flex flex-column align-items-center justify-content-center" style="max-width: 26rem;">
+        <h1 class="text-center text-6xl my-7">Welcome again!</h1>
+        <div class="m-3 text-center">
+            <div class="px-3">
+                <span class="p-float-label">
+                    <pv-input-text />
+                    <label for="username">Username</label>
+                </span>
+            </div>
+        </div>
 
         <div class="m-3 text-center">
-        <div class="px-3">
-            <span class="p-inputgroup-addon inline">
-                <i class="pi pi-lock"></i>
-            </span>
-            <pv-input-text placeholder="Password" class="pr-8" type="password" />
+            <div class="px-3">
+                <span class="p-float-label">
+                    <pv-input-text type="password" />
+                    <label for="password">Password</label>
+                </span>
+            </div>
         </div>
+        <div>
+            <span>Forgot password?</span>
+        </div>
+        <div class="text-xl flex flex-column m-3">
+            <div class="field-radiobutton w-full">
+                <pv-radio-button id="option1" name="user" value="home-customer" v-model="user" />
+                <label for="city1">I am a driver</label>
+            </div>
+            <div class="field-radiobutton w-full">
+                <pv-radio-button id="option2" name="user" value="home-mechanic" v-model="user" />
+                <label for="city1">I am a mechanic</label>
+            </div>
+        </div>
+        <div class="button w-full text-center my-5">
+                <RouterLink to="/home-customer">
+                    <pv-button class="w-full" label = "Sign In" />
+                </RouterLink>
+        </div>
+    <div>
+        <span>Don't have an account? </span>
+        <a href="">Sign up!</a>
     </div>
-<div class="text-xl flex flex-column m-3">
-    <div class="field-radiobutton w-full">
-        <pv-radio-button id="option1" name="user" value="home-customer" v-model="user" />
-        <label for="city1">I am a driver</label>
-    </div>
-    <div class="field-radiobutton w-full">
-            <pv-radio-button id="option2" name="user" value="home-mechanic" v-model="user" />
-            <label for="city1">I am a mechanic</label>
-    </div>
-</div>
-    <div class="button">
-            <RouterLink to="/home-customer">
-                <pv-button label = "Sign In" class = "p-button-rounded p-button-lg w-full" />
-            </RouterLink>
+
+
+        </div>
     </div>
 </div>
 </template>
@@ -53,4 +69,7 @@ export default {
 
 <style>
 
+.grid{
+    grid-gap:0 !important;
+}
 </style>
