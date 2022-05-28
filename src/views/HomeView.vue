6f2b61a7524f9deebb5d576be8030ae5587c9ade
oscar14pm <44507194+oscar14pm@users.noreply.github.com>
2022-05-28 16:50:31 -0500

    fix: writing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <RouterView />
   <div class="home">
     <h1 class="mr-2">Mecanillama</h1>
-    <p>The app for connecting drivers and workshops</p>
+    <p>The app that connects drivers and workshops</p>
     <div class="buttons">
       <div class="button">
         <RouterLink to="/sign-up">
