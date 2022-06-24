6347510e352b5eab6062c416574625ec3b17468f
Rojoxt <64550279+Rojoxt@users.noreply.github.com>
2022-06-23 23:12:33 -0500

    feat(mechanic):refactor mechanic header

--- src/components/mechanic-header.component.vue
+++ src/components/mechanic-header.component.vue
@@ -13,11 +13,8 @@
     <template #end>
         <div class="flex">
         <div class="m-3 search-input w-full">
-            <pv-input-text
-                v-model="search"
-                class="border-round p-3 px-3 w-full"
-                placeholder="Search here..." />
-        </div>    
+
+        </div>
             <div class="flex align-items-center justify-content-center cursor-pointer profile text-center">
                 <menu-bar :model="accountMenu"></menu-bar>
                 <pv-avatar image="https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&auto=format&fit=crop&w=128"  class="mr-2" size="large" shape="circle" />
@@ -36,14 +33,12 @@ export default{
     data(){
         return{
             navigation:[
-                { label: "Home",icon:'pi pi-fw pi-home', to: "/home-customer" },
-                { label: "Appointments", icon:'pi pi-fw pi-calendar',to: "/appointments-customer" },
-                { label: "Favourites", icon:'pi pi-fw pi-star', to: "/favourites" },
+                { label: "Home",icon:'pi pi-fw pi-home', to: "/mechanic-admin/home" },
                 { label: "Notifications", icon:'pi pi-fw pi-bell', to: "/notifications" },
             ],
             accountMenu: [{
                 items: [
-                    { label: "Profile", to: "/customer-profile", icon: 'pi pi-fw pi-user'},
+                    { label: "Profile", to: "/mechanic-admin/home", icon: 'pi pi-fw pi-user'},
                     { label: "Sign out", to: "/", icon: 'pi pi-fw pi-sign-out' },
                 ]
             }
